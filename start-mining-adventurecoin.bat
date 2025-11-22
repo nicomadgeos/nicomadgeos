08890fd90e8e8ddbf1c2b63e085b18c35446b2a4
nicomadgeos <rasp.orbiter5b@icloud.com>
2025-11-22 19:55:54 +0200

    Add files via upload

---
+++ start-mining-adventurecoin.bat
@@ -0,0 +1,9 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --algorithm yespoweradvc --pool stratum-eu.rplant.xyz:7149 --wallet advc-wallet
+pause
